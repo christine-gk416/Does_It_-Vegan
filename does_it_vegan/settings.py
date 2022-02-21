2a8c17dd7212b46585edcd011e292859d0757319
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-02-21 21:28:03 +0000

    fix syntax error in alowed host

--- does_it_vegan/settings.py
+++ does_it_vegan/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['does_it_vegan.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['does-it-vegan.herokuapp.com', 'localhost']
 
 
 # Application definition
