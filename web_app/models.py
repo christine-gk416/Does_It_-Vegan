c30c33b3e40333d2e537457f5b00644d1a71350e
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-02-21 22:29:44 +0000

    add basic model for restaurant

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Authorised"))
+
+class Restaurants(models.Model):
+    """
+    model for restaurant data
+    """
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="restaurant_added")
+    updated_on = models.DateField(auto_now=True)
