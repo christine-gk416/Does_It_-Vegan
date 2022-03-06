9f448e718987ba13fb53a23b627cc07ef226de0d
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-03-06 20:16:25 +0000

    change if statment for database in settings.py

--- does_it_vegan/settings.py
+++ does_it_vegan/settings.py
@@ -88,17 +88,18 @@ WSGI_APPLICATION = 'does_it_vegan.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if development:
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
