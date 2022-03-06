7dc073ffc34a45f964b79b057356fba8646ddf4a
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-03-06 17:27:49 +0000

    change debug setting to get heroku deplyment working

--- does_it_vegan/settings.py
+++ does_it_vegan/settings.py
@@ -14,11 +14,11 @@ from pathlib import Path
 import os
 
 import dj_database_url
-if os.path.isfile('env.py'):
-    import env
+# if os.path.isfile('env.py'):
+#     import env
 
 
-development = os.environ.get('DEVELOPMENT', False)
+# development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = True
 
 ALLOWED_HOSTS = ['does-it-vegan.herokuapp.com', 'localhost']
 
