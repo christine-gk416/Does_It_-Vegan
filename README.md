d0a85201b5e4802b50e1d0b194a73036e9064484
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-03-16 22:57:51 +0000

    add dish model to context in restrauntList view and add dishes to index.html

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ tech
 
 images-
 usplash
-https://unsplash.com/photos/GXXYkSwndP4 
+restaraunt-image-1 https://unsplash.com/photos/GXXYkSwndP4 
+chipy - https://unsplash.com/photos/YOW3de4wEDk
 
 acknolege emnts
 dave and dasiy mentor - deployment
--- web_app/views.py
+++ web_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Restaurant
+from django.views import generic, View
+from .models import Restaurant, Dish
 
 class RestaurantList(generic.ListView):
     """
@@ -10,3 +10,10 @@ class RestaurantList(generic.ListView):
     queryset = Restaurant.objects.filter(status=1).order_by('-updated_on')
     template_name = 'index.html'
     paginate_by = 6
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['dish'] = Dish.objects.all()
+        return context
