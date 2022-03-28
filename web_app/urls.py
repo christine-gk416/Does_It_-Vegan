6cf0a858a23f14b65d97139369f2b05b67b72fc4
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-03-28 23:14:00 +0000

    add sign up path to url.py

--- web_app/urls.py
+++ web_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.RestaurantList.as_view(), name='home'),
     path("search/", views.SearchResultsView.as_view(), name="search_results"),
+    path("sign-up/", views.SearchResultsView.as_view(), name="sign_up"),
 ]
