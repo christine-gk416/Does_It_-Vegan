a973dd6148adef3d2badf65b2bc802be9199707e
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-04-29 22:29:45 +0000

    add new static root in setting.py to try fixing cloudenry issues

--- does_it_vegan/settings.py
+++ does_it_vegan/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 MEDIA_URL = '/media/'
