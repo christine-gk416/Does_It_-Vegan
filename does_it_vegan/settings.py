9445ff0f89d27c9a7c5a2e7477b61e091724062d
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-04-29 21:53:53 +0000

    add cloudinary url to settings.py

--- does_it_vegan/settings.py
+++ does_it_vegan/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
