9f030809a44f133a68d2212024462942c644736d
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-04-30 07:20:42 +0000

    add comma to staticfiles_dirs and change order of apps in settings.py

--- does_it_vegan/settings.py
+++ does_it_vegan/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'cloudinary_storage',
-    'cloudinary',
     'django.contrib.staticfiles',
+    'cloudinary',
     'django_summernote',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
